bc02a7d257b0ebf7c620e6ac4fb028fc6cf4d993
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-04-10 14:35:19 +0300

    Added GraphService

---
+++ WebApplication/Services/IGraphService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Services
+{
+    public interface IGraphService
+    {
+        MessageRequest BuildEmailMessage(string recipients, string subject);
+
+        Task<string> GetMyEmailAddress(string accessToken);
+
+        Task<string> SendEmail(string accessToken, MessageRequest email);
+    }
+}
