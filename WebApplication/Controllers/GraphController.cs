c826ac54e9231e9d3f30e60ad6f3a1531d1d652c
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-04-10 14:43:47 +0300

    Added Ninject

---
+++ WebApplication/Controllers/GraphController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+using WebApplication.Services;
+
+namespace WebApplication.Controllers
+{
+    public class GraphController : Controller
+    {
+        private readonly IGraphService service;
+
+        public GraphController(IGraphService service)
+        {
+            if (service == null)
+            {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            this.service = service;
+        }
+
+        // GET: Graph
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ WebApplication/Settings/NinjectBindings.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication.Settings
+{
+    using Ninject.Extensions.Conventions;
+    using Ninject.Modules;
+
+    public class NinjectBindings : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind(configure =>
+            {
+                configure.FromThisAssembly()
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
+        }
+    }
+}
