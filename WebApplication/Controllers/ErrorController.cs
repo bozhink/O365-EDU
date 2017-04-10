92de42ff5d36ac76bd756f139a836aa57ca6edec
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-04-10 14:46:08 +0300

    Added ErrorController

---
+++ WebApplication/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index(string message)
+        {
+            this.ViewBag.Message = message;
+            return this.View("Error");
+        }
+    }
+}
