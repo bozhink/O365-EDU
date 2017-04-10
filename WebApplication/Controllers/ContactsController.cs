32edfa4f3b1df5b306230f4629cc2d8cbca3478c
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-04-10 14:27:08 +0300

    Added ContactsController.AddContact

--- WebApplication/Controllers/ContactsController.cs
+++ WebApplication/Controllers/ContactsController.cs
@@ -17,6 +17,31 @@ namespace WebApplication.Controllers
 
             var client = await ClientFactory.GreateOutlookServicesClientAsync("Contacts");
 
+            // Fetch
+            var contactsResult = await client.Me.Contacts.ExecuteAsync();
+
+            do
+            {
+                var contacts = contactsResult.CurrentPage;
+                foreach (var contact in contacts)
+                {
+                    contactList.Add(new ContactViewModel
+                    {
+                        Name = contact.DisplayName
+                    });
+                }
+
+                contactsResult = await contactsResult.GetNextPageAsync();
+            } while (contactsResult != null);
+
+            return this.View(contactList);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddContact()
+        {
+            var client = await ClientFactory.GreateOutlookServicesClientAsync("Contacts");
+
             Contact newContact = new Contact
             {
                 GivenName = "Katie",
@@ -39,24 +64,14 @@ namespace WebApplication.Controllers
             // Get the contact ID.
             string contactId = newContact.Id;
 
-            // Fetch
-            var contactsResult = await client.Me.Contacts.ExecuteAsync();
-
-            do
+            return new JsonResult
             {
-                var contacts = contactsResult.CurrentPage;
-                foreach (var contact in contacts)
+                ContentType = "application/json",
+                Data = new
                 {
-                    contactList.Add(new ContactViewModel
-                    {
-                        Name = contact.DisplayName
-                    });
+                    id = contactId
                 }
-
-                contactsResult = await contactsResult.GetNextPageAsync();
-            } while (contactsResult != null);
-
-            return this.View(contactList);
+            };
         }
     }
 }
