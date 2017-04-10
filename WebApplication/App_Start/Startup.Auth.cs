05ce8c53f687663df7cc12099d15f0568529959d
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-04-10 15:04:11 +0300

    Startup.Auth configurations

--- WebApplication/App_Start/Startup.Auth.cs
+++ WebApplication/App_Start/Startup.Auth.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Globalization;
 using System.IdentityModel.Claims;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Owin;
 using WebApplication.Models;
 
@@ -18,19 +15,20 @@ namespace WebApplication
     public partial class Startup
     {
         private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
-        private static string appKey = ConfigurationManager.AppSettings["ida:ClientSecret"];
+        private static string clientSecret = ConfigurationManager.AppSettings["ida:ClientSecret"];
         private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
         private static string tenantId = ConfigurationManager.AppSettings["ida:TenantId"];
-        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+        private static string redirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+        private static string graphScopes = ConfigurationManager.AppSettings["ida:GraphScopes"];
 
         public static readonly string Authority = aadInstance + tenantId;
 
         // This is the resource ID of the AAD Graph API.  We'll need this to request a token to call the Graph API.
-        string graphResourceId = "https://graph.windows.net";
+        private string graphResourceId = "https://graph.windows.net";
 
         public void ConfigureAuth(IAppBuilder app)
         {
-            ApplicationDbContext db = new ApplicationDbContext();
+            ////ApplicationDbContext db = new ApplicationDbContext();
 
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
 
@@ -41,19 +39,46 @@ namespace WebApplication
                 {
                     ClientId = clientId,
                     Authority = Authority,
-                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    PostLogoutRedirectUri = redirectUri,
+                    RedirectUri = redirectUri,
+                    Scope = "openid email profile offline_access " + graphScopes,
 
+                    TokenValidationParameters = new System.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        // In a real application you would use IssuerValidator for additional checks, 
+                        // like making sure the user's organization has signed up for your app.
+                        //     IssuerValidator = (issuer, token, tvp) =>
+                        //     {
+                        //         if (MyCustomTenantValidation(issuer)) 
+                        //             return issuer;
+                        //         else
+                        //             throw new SecurityTokenInvalidIssuerException("Invalid issuer");
+                        //     },
+                    },
                     Notifications = new OpenIdConnectAuthenticationNotifications()
                     {
                         // If there is a code in the OpenID Connect response, redeem it for an access token and refresh token, and store those away.
                         AuthorizationCodeReceived = async (context) =>
                         {
                             var code = context.Code;
-                            ClientCredential credential = new ClientCredential(clientId, appKey);
                             string signedInUserID = context.AuthenticationTicket.Identity.FindFirst(ClaimTypes.NameIdentifier).Value;
-                            AuthenticationContext authContext = new AuthenticationContext(Authority, new ADALTokenCache(signedInUserID));
-                            AuthenticationResult result = await authContext.AcquireTokenByAuthorizationCodeAsync(
-                            code, new Uri(HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Path)), credential, graphResourceId);
+
+                            var credential = new ClientCredential(clientId, clientSecret);
+
+                            var authenticationContext = new AuthenticationContext(Authority, new ADALTokenCache(signedInUserID));
+
+                            var result = await authenticationContext.AcquireTokenByAuthorizationCodeAsync(
+                                code,
+                                new Uri(HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Path)),
+                                credential,
+                                graphResourceId);
+                        },
+                        AuthenticationFailed = (context) =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("/Error?message=" + context.Exception.Message);
+                            return Task.FromResult(0);
                         }
                     }
                 });
